d1156edd9fdaac0de8b4e819b926d354b35c8af0
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2024-05-13 10:11:40 +0200

    Update era5process.py

--- roles/prep_shared_data/files/era5process.py
+++ roles/prep_shared_data/files/era5process.py
@@ -35,6 +35,8 @@ ds = Dataset(
 ds.find_files()
 xds = xarray.open_dataset(ds.first_file, chunks=auto)
 encoding = xsd[args.era5_name].encoding
+rawunit = xsd[args.era5_name].attrs.unit
+xds.close()
 cube = ds.load()
 
 # Do + first day of next year
@@ -43,13 +45,38 @@ cube = extract_time(cube, start=f'{current_year}-01-01', end=f'{current_year + 1
 cube = daily_statistics(cube, operator='mean')
 # TODO for taxmin and taxmax, we need to do the daily max and min
 
+def get_encoding(data: xr.DataArray) -> dict[str, str]:
+    vmin = data.min()
+    vmax = data.max()
+    offset = (vmax - vmin)/2 + vmin
+    scale_factor = np.max([vmin - offset, offset - vmin])/2**15 * 1.01 # room for nans
+ 
+    return {
+        "dtype": np.dtype("int16"),
+        "missing_value": -32767,
+        "_FillValue": -32767,
+        "scale_factor": float(scale_factor),
+        "add_offset": float(offset),
+        "zlib": True,
+        "complevel": 7,
+        "shuffle": True,
+        # Chunksize time in single chunk or -1, chunks around 10Mb
+        "chunksizes": (24, 40, 70),
+    }
+ 
+encoding = {}
+for var in ds.data_vars:
+    encoding[var] = get_encoding(ds[var])
+
 # Save file
-xds2 = cube.to_xarray()
+xds2 = xarray.from_iris(cube)
 encoding[zlib] = true
 enconding[complevel] = 5 
-if unit_thesame:
-    xsd2[args.era5_name].encoding = encoding
+newunit = xsd2[args.short_name].attrs.unit
+if newunit != oldunit:
+    xsd2[args.short_name].encoding = encoding
 else:
+    xsd2[args.short_name].encoding = get_encoding( xsd2[args.era5_name])
     # derive center / offset 
 xds2.to_netcdf(args.output)
 # save(cube, args.output, zlib=True)
